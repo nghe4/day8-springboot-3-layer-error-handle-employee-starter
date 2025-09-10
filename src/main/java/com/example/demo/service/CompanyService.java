75a84a28e1f66ff35d7a17fff660d611cc766af0
nghe4 <nghe4@oocl.com>
2025-09-10 11:44:41 +0800

    feat: add CompanyService class

---
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Company;
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.CompanyRepository;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+}

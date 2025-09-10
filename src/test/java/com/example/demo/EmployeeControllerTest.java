e93144ae5c6024a0dde2bf411843783dfa1d6e5a
nghe4 <nghe4@oocl.com>
2025-09-10 10:50:39 +0800

    refactor(EmployeeControllerTest): should_status_204_when_delete_employee

--- src/test/java/com/example/demo/EmployeeControllerTest.java
+++ src/test/java/com/example/demo/EmployeeControllerTest.java
@@ -146,15 +146,15 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$[0].gender").value(expect.getGender()))
                 .andExpect(jsonPath("$[0].salary").value(expect.getSalary()));
     }
-//
-//    @Test
-//    void should_status_204_when_delete_employee() throws Exception {
-//        int id = employeeController.createEmployee(johnSmith()).getId();
-//
-//        mockMvc.perform(delete("/employees/" + id))
-//                .andExpect(status().isNoContent());
-//    }
-//
+
+    @Test
+    void should_status_204_when_delete_employee() throws Exception {
+        int id = createJohnSmith().getId();
+
+        mockMvc.perform(delete("/employees/" + id))
+                .andExpect(status().isNoContent());
+    }
+
 //    @Test
 //    void should_status_200_when_update_employee() throws Exception {
 //        Employee expect = employeeController.createEmployee(johnSmith());

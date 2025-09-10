dd53cb570577062eed28fb1012608d22f5d072f8
nghe4 <nghe4@oocl.com>
2025-09-10 17:48:34 +0800

    refactor(GlobalExceptionHandler): replace ControllerAdvice with RestControllerAdvice

--- src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -7,9 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResponseStatusException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)

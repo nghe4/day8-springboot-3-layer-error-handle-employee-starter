e4c77714737c61f1e740d86940d004c6a279e735
nghe4 <nghe4@oocl.com>
2025-09-10 16:37:02 +0800

    feat: add GlobalExceptionHandler class

---
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class GlobalExceptionHandler {
+
+}

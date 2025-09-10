ff0c261e4f823a3da78a28c4911dd9a8b8a74784
nghe4 <nghe4@oocl.com>
2025-09-10 13:52:36 +0800

    feat: add InvalidAgeEmployeeException class

---
+++ src/main/java/com/example/demo/Exception/InvalidAgeEmployeeException.java
@@ -0,0 +1,7 @@
+package com.example.demo.Exception;
+
+public class InvalidAgeEmployeeException extends Exception{
+    public InvalidAgeEmployeeException(String message) {
+        super(message);
+    }
+}

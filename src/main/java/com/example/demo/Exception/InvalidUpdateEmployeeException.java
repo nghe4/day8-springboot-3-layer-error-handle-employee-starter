a66cdcacd69a2823a4446db015c9b0bdb3bec7cb
nghe4 <nghe4@oocl.com>
2025-09-10 15:27:54 +0800

    feat: add InvalidUpdateEmployeeException class

---
+++ src/main/java/com/example/demo/Exception/InvalidUpdateEmployeeException.java
@@ -0,0 +1,7 @@
+package com.example.demo.Exception;
+
+public class InvalidUpdateEmployeeException extends Exception{
+    public InvalidUpdateEmployeeException(String message) {
+        super(message);
+    }
+}

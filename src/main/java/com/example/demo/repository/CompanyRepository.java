208bdf1903c746388465cc439c6bec70b24354fb
nghe4 <nghe4@oocl.com>
2025-09-10 11:44:28 +0800

    feat: add CompanyRepository class

---
+++ src/main/java/com/example/demo/repository/CompanyRepository.java
@@ -0,0 +1,22 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Company;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Repository
+public class CompanyRepository {
+    private final List<Company> companies = new ArrayList<>();
+
+    public CompanyRepository() {
+        companies.add(new Company(1, "Spring"));
+        companies.add(new Company(2, "Autumn"));
+        companies.add(new Company(3, "Winter"));
+        companies.add(new Company(4, "Summer"));
+    }
+}

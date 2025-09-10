7838989ea570ec1e4fd33f171df78ca3efe45e68
nghe4 <nghe4@oocl.com>
2025-09-10 13:48:06 +0800

    feat: add EmployeeServiceTest class

---
+++ src/test/java/com/example/demo/EmployeeServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class EmployeeServiceTest {
+}

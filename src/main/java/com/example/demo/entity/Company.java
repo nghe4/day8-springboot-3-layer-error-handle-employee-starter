ef9de095d5d92bfe79f29ad4f890a8d2e14fad3e
nghe4 <nghe4@oocl.com>
2025-09-11 13:14:23 +0800

    feat(Company): update with JPA

--- src/main/java/com/example/demo/entity/Company.java
+++ src/main/java/com/example/demo/entity/Company.java
@@ -1,12 +1,22 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
     public Company(Integer id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Company() {
+
     }
 
     public Integer getId() {

c5c942435e84cae4ad500f97aafc5fe2a55a271b
nghe4 <nghe4@oocl.com>
2025-09-11 11:58:19 +0800

    refactor(EmployeeServiceTest): update all testcases with IEmployeeRepository & JPA

--- src/test/java/com/example/demo/EmployeeServiceTest.java
+++ src/test/java/com/example/demo/EmployeeServiceTest.java
@@ -4,13 +4,15 @@ import com.example.demo.entity.Employee;
 import com.example.demo.exceptions.InvalidAgeEmployeeException;
 import com.example.demo.exceptions.InvalidSalaryEmployeeException;
 import com.example.demo.exceptions.InvalidUpdateEmployeeException;
-import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.repository.IEmployeeRepository;
 import com.example.demo.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -23,12 +25,12 @@ public class EmployeeServiceTest {
     private EmployeeService employeeService;
 
     @Mock
-    private EmployeeRepository employeeRepository;
+    private IEmployeeRepository employeeRepository;
 
     @Test
     void should_throw_exception_when_create_a_employee() throws InvalidSalaryEmployeeException, InvalidAgeEmployeeException {
         Employee employee = new Employee(null, "Tom", 29, "MALE", 20000.0);
-        when(employeeRepository.createEmployee(any(Employee.class))).thenReturn(employee);
+        when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
         Employee employeeResult = employeeService.createEmployee(employee);
 
         assertEquals(employeeResult.getAge(), employee.getAge());
@@ -37,21 +39,21 @@ public class EmployeeServiceTest {
     @Test
     void should_throw_exception_when_create_a_employee_of_age_greater_than_65_or_less_than_18() {
         Employee employee = new Employee(null, "Tom", 16, "MALE", 20000.0);
-        when(employeeRepository.createEmployee(any(Employee.class))).thenReturn(employee);
+        when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
         assertThrows(InvalidAgeEmployeeException.class, () -> employeeService.createEmployee(employee));
     }
 
     @Test
     void should_throw_exception_when_create_a_employee_of_age_over_29_and_salary_below_20000() {
         Employee employee = new Employee(null, "Tom", 31, "MALE", 10000.0);
-        when(employeeRepository.createEmployee(any(Employee.class))).thenReturn(employee);
+        when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
         assertThrows(InvalidSalaryEmployeeException.class, () -> employeeService.createEmployee(employee));
     }
 
     @Test
     void should_set_active_status_true_when_create_a_employee() {
         Employee employee = new Employee(null, "Tom", 18, "MALE", 10000.0);
-        when(employeeRepository.createEmployee(any(Employee.class))).thenReturn(employee);
+        when(employeeRepository.save(any(Employee.class))).thenReturn(employee);
         assertTrue(employee.isActive());
     }
 
@@ -59,18 +61,18 @@ public class EmployeeServiceTest {
     void should_set_active_status_false_when_delete_a_employee() {
         Employee employee = new Employee(1, "Tom", 20, "MALE", 10000.0);
         assertTrue(employee.isActive());
-        when(employeeRepository.getEmployeeById(1)).thenReturn(employee);
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
 
         employeeService.deleteEmployee(1);
 
-        verify(employeeRepository).updateEmployee(eq(1), argThat(e -> e.isActive() == false));
+        verify(employeeRepository).save(argThat(e -> !e.isActive()));
     }
 
     @Test
     void should_throw_exception_when_update_a_employee_with_active_status_false() {
         Employee employee = new Employee(null, "Tom", 18, "MALE", 20000.0);
         employee.setActive(false);
-        when(employeeRepository.getEmployeeById(1)).thenReturn(employee);
+        when(employeeRepository.findById(1)).thenReturn(Optional.of(employee));
 
         assertThrows(InvalidUpdateEmployeeException.class, () -> employeeService.updateEmployee(1, employee));
     }

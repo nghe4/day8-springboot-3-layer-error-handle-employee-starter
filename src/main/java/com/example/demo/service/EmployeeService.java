bf47f171daf03f6e1ccbf0de1faa506bef3b58ab
nghe4 <nghe4@oocl.com>
2025-09-11 11:16:43 +0800

    refactor: createEmployee with JPA

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -56,9 +56,11 @@ public class EmployeeService {
             throw new InvalidAgeEmployeeException("Employee age is null");
         if (employee.getAge() < 18 || employee.getAge() > 65)
             throw new InvalidAgeEmployeeException("Employee age less than 18 or greater than 65");
-        if (employee.getAge() > 29 && (employee.getSalary() == null || employee.getSalary() < 20000))
+        if (employee.getSalary() == null || employee.getSalary() <= 0)
+            throw new InvalidSalaryEmployeeException("Employee salary is null or less than or equal to 0");
+        if (employee.getAge() > 29 && employee.getSalary() < 20000)
             throw new InvalidSalaryEmployeeException("Employee age over 29 and salary below 20000");
-        return employeeRepository.createEmployee(employee);
+        return employeeRepository.save(employee);
     }
 
     public Employee updateEmployee(int id, Employee updatedEmployee) throws InvalidUpdateEmployeeException {

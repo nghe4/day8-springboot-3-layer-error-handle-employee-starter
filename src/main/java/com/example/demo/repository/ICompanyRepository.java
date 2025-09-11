635618c169596710b4b2a6829b1cb897b6109719
nghe4 <nghe4@oocl.com>
2025-09-11 13:33:23 +0800

    refactor(ICompanyRepository): remove unnecessary import

--- src/main/java/com/example/demo/repository/ICompanyRepository.java
+++ src/main/java/com/example/demo/repository/ICompanyRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Company;
-import com.example.demo.entity.Employee;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface ICompanyRepository extends JpaRepository<Company, Integer> {
 }

af5d730d6f4bbfeb681d1f2f832b8402c5741bbc
nghe4 <nghe4@oocl.com>
2025-09-11 13:03:27 +0800

    refactor(CompanyService): update all method with ICompanyRepository & JPA

--- src/main/java/com/example/demo/service/CompanyService.java
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -1,40 +1,57 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Company;
-import com.example.demo.repository.CompanyRepository;
+import com.example.demo.entity.Employee;
+import com.example.demo.exceptions.InvalidUpdateEmployeeException;
+import com.example.demo.repository.ICompanyRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyService {
-    private final CompanyRepository companyRepository;
+    private final ICompanyRepository companyRepository;
 
-    public CompanyService(CompanyRepository companyRepository) {
+    public CompanyService(ICompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
 
-    public void clearCompanies() {
-        companyRepository.clearCompanies();
-    }
-
     public List<Company> getCompanies(Integer page, Integer size) {
-        return companyRepository.getCompanies(page, size);
+        if (page == null || size == null) {
+            return companyRepository.findAll();
+        }
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return companyRepository.findCompaniesByPageable(pageable).stream().toList();
     }
 
     public Company createCompany(Company company) {
-        return companyRepository.createCompany(company);
+        return companyRepository.save(company);
     }
 
     public Company getCompanyById(int id) {
-        return companyRepository.getCompanyById(id);
+        Optional<Company> company = companyRepository.findById(id);
+        if (company.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found with id: " + id);
+        }
+        return company.get();
     }
 
     public Company updateCompany(int id, Company updatedCompany) {
-        return companyRepository.updateCompany(id, updatedCompany);
+        Optional<Company> company = companyRepository.findById(id);
+
+        if (company.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found with id: " + id);
+        }
+        updatedCompany.setId(id);
+        return companyRepository.save(updatedCompany);
     }
 
     public void deleteCompany(int id) {
-        companyRepository.deleteCompany(id);
+        companyRepository.deleteById(id);
     }
 }

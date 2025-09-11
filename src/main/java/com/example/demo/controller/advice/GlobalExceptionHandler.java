e2b5f0aea593fb8be49f534258b3405ac2149533
nghe4 <nghe4@oocl.com>
2025-09-11 16:25:14 +0800

    feat(GlobalExceptionHandler): exception handler for MethodArgumentNotValidException

--- src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -3,12 +3,15 @@ package com.example.demo.controller.advice;
 import com.example.demo.exceptions.InvalidUpdateEmployeeException;
 import com.example.demo.exceptions.InvalidAgeEmployeeException;
 import com.example.demo.exceptions.InvalidSalaryEmployeeException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +19,15 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseException exceptionHandler(Exception e) {
         return new ResponseException(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseException handlerArgumentNotValid(MethodArgumentNotValidException exception) {
+        String errorMessage = exception.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(" | "));
+
+        return new ResponseException(errorMessage);
     }
 
     @ExceptionHandler({InvalidUpdateEmployeeException.class, InvalidAgeEmployeeException.class, InvalidSalaryEmployeeException.class})
